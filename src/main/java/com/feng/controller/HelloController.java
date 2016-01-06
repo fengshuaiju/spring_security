843a87fe2a5ce6b5e9f1c059eedf0a0f111940b0
fengshuaiju <1179694483@qq.com>
2016-01-06 23:40:25 +0800

    设置登录页面 获取 token

--- src/main/java/com/feng/controller/HelloController.java
+++ src/main/java/com/feng/controller/HelloController.java
@@ -1,6 +1,13 @@
 package com.feng.controller;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +29,29 @@ public class HelloController {
 		
 		model.addAttribute("say",say);
 		
+		this.getCurrentUser();
+		
 		return "hello/hello";
+	}
+	
+	
+	public void getCurrentUser(){
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+		
+		String name = authentication.getName();//获取当前登录用户姓名
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();//获取当前用户的所有权限
+		Object principal = authentication.getPrincipal();
+		Object details = authentication.getDetails();
+		
+		WebAuthenticationDetails authenticationDetails = (WebAuthenticationDetails) authentication.getDetails();
+		
+		String token = authenticationDetails.getSessionId();
+		
+		Object credentials = authentication.getCredentials();
+		boolean authenticated = authentication.isAuthenticated();
+		
+		System.out.println(token);
+		
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,11 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" import="java.util.*" %>  
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
 <html>
 <body>
 <h2>Hello World!!!</h2>
-<a href="/sayHello">click me</a>
+<a href="<%=basePath%>sayHello">click me</a>
 </body>
 </html>
