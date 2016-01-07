52856b9d2a7f952e921dffa682bb380e26861e32
fengshuaiju <1179694483@qq.com>
2016-01-07 23:26:20 +0800

    xxxxx

---
+++ src/main/java/com/feng/repositories/ModelRepositorie.java
@@ -0,0 +1,9 @@
+package com.feng.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.feng.model.ModelEntity;
+
+public interface ModelRepositorie extends PagingAndSortingRepository<ModelEntity,Long>{
+
+}
--- src/main/java/com/feng/service/HelloService.java
+++
@@ -1,5 +0,0 @@
-package com.feng.service;
-
-public interface HelloService {
-	public String say(String name);
-}
